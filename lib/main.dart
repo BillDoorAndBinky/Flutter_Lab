a84b6d165738906be3a5e8d74d8944595a91a60c
BillDoorAndBinky <DYROGONICHE.doc@gmail.com>
2022-03-08 01:44:53 +0300

    #2_Добавил Стек

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: const Text(
-        "Проект студента группы 521828",
-        style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            shadows: [Shadow(color: Colors.black, blurRadius: 30)]),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Проект студента группы 521828",
+          style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              shadows: [Shadow(color: Colors.black, blurRadius: 30)]),
+        ),
       ),
+      body: Container(
+          decoration: const BoxDecoration(
+            color: Colors.green,
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: const <Widget>[
+              Icon(Icons.adjust, size: 150, color: Colors.redAccent),
+              Icon(Icons.adjust, size: 100, color: Colors.amber),
+              Icon(Icons.adjust, size: 50, color: Colors.teal),
+              Positioned(
+                  top: 100,
+                  child: Text('TEST', style: TextStyle(fontSize: 30))),
+            ],
+          )),
     ),
-    body: Container(
-      decoration: const BoxDecoration(
-        color: Colors.green,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: const <Widget>[
-          Icon(Icons.adjust, size: 30, color: Colors.teal),
-          Icon(Icons.adjust, size: 50, color: Colors.redAccent),
-          Icon(Icons.adjust, size: 30, color: Colors.teal),
-        ],
-      ),
-    ),
-  )));
+  ));
 }
